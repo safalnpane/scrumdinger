89f5fe974e070b9ed1aedef8d9e21d4fc11679cf
Safal Neupane <safal.npane@gmail.com>
2022-10-04 15:26:38 +0545

    Can list cards with background

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Safal Neupane on 4/10/2022.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
